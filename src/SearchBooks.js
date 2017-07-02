956d86daa865bfa0ed275ada5350c57b603f4f6d
Ashis Pavan <ashispavan@live.com>
2017-07-03 00:01:53 +0530

    Added Router Functionality

---
+++ src/SearchBooks.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class SearchBooks extends Component {
+
+
+
+    render() {
+        const {books} = this.props;
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author"/>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                    {books.map(book => 
+                    <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors[0]}</div>
+                        </div>
+                    </li>
+                    )}
+                    </ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBooks;
